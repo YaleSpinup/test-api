91384037af27f5c03bf9c894e4b2f20f31815b8b
E Camden Fisher <fish@fishnix.net>
2020-01-27 16:43:05 -0500

    add metadata endpoints

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -13,6 +14,9 @@ func main() {
 
 	http.HandleFunc("/ping", pingPongHandler)
 	http.HandleFunc("/env", envHandler)
+	http.HandleFunc("/metadata/task", metadataTaskHandler)
+	http.HandleFunc("/metadata/stats", metadataStatsHandler)
+	http.HandleFunc("/metadata/task/stats", metadataTaskStatsHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
@@ -27,6 +31,77 @@ func pingPongHandler(w http.ResponseWriter, req *http.Request) {
 func envHandler(w http.ResponseWriter, req *http.Request) {
 	e := os.Environ()
 	_, err := io.WriteString(w, strings.Join(e, "\n"))
+	if err != nil {
+		log.Printf("error writing: %s", err)
+	}
+}
+
+func metadataTaskHandler(w http.ResponseWriter, req *http.Request) {
+	metadataUrl := os.Getenv("ECS_CONTAINER_METADATA_URI")
+	if metadataUrl == "" {
+		w.WriteHeader(http.StatusNotImplemented)
+		w.Write([]byte("metadata not found"))
+		return
+	}
+
+	client := &http.Client{
+		Timeout: time.Second * 3,
+	}
+	out, err := client.Get(metadataUrl + "/task")
+	if err != nil {
+		log.Printf("error getting metadata: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	_, err = io.Copy(w, out.Body)
+	if err != nil {
+		log.Printf("error writing: %s", err)
+	}
+}
+
+func metadataStatsHandler(w http.ResponseWriter, req *http.Request) {
+	metadataUrl := os.Getenv("ECS_CONTAINER_METADATA_URI")
+	if metadataUrl == "" {
+		w.WriteHeader(http.StatusNotImplemented)
+		w.Write([]byte("metadata not found"))
+		return
+	}
+
+	client := &http.Client{
+		Timeout: time.Second * 3,
+	}
+	out, err := client.Get(metadataUrl + "/stats")
+	if err != nil {
+		log.Printf("error getting metadata: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	_, err = io.Copy(w, out.Body)
+	if err != nil {
+		log.Printf("error writing: %s", err)
+	}
+}
+func metadataTaskStatsHandler(w http.ResponseWriter, req *http.Request) {
+	metadataUrl := os.Getenv("ECS_CONTAINER_METADATA_URI")
+	if metadataUrl == "" {
+		w.WriteHeader(http.StatusNotImplemented)
+		w.Write([]byte("metadata not found"))
+		return
+	}
+
+	client := &http.Client{
+		Timeout: time.Second * 3,
+	}
+	out, err := client.Get(metadataUrl + "/task/stats")
+	if err != nil {
+		log.Printf("error getting metadata: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	_, err = io.Copy(w, out.Body)
 	if err != nil {
 		log.Printf("error writing: %s", err)
 	}
